c00a1d46e60435c718c4f205490b82b23877787c
joel <joeljsaleem@gmail.com>
2020-04-28 21:44:02 +0100

    remove unused variables

--- client/src/components/List/ListContainer.js
+++ client/src/components/List/ListContainer.js
@@ -3,19 +3,6 @@ import styled from "../../modules/styled";
 import { mapAudioDataToBtns, fetchAudioList } from "./eventHandlers";
 import { USE_SERVER } from "../../App";
 
-export const localAudioFiles = [
-  {
-    title: "TOL_6min_720p_download",
-    audioUrl: "TOL_6min_720p_download.wav",
-    waveformUrl: "TOL_6min_720p_download.json",
-  },
-  {
-    title: "07023003",
-    audioUrl: "07023003.wav",
-    waveformUrl: "07023003.json",
-  },
-];
-
 const ListWrapper = styled.div`
   display: flex;
   width: 100%;
